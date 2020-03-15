0b44ef59be949abbda70e7b203b24800b1586fc2
rishidhiman <rishidhiman10@gmail.com>
2020-03-15 19:14:43 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var description = new Typed('#description', {
+        strings: ['Tech Enthusiast^3000', 'Programmer', 'website Developer', 'Gadget lover', 'YouTube Creator', 'android Enthusiast'],
+        shuffle: true,
+        loop: true,
+        typeSpeed: 50,
+        backSpeed: 50,
+        cursorChar: '_',
+    })
+})
